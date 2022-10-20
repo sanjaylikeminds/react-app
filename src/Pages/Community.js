7b10bb8978cc5f557e841d09bfde6c9f9b8bb71d
Sanjay Kumar <sanjay.kumar@likeminds.community>
2022-10-20 13:49:39 +0530

    sdk bug fixed

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Community() {
   return (
-    <div>
+    <div className="likemind-community">
       <likeminds-web-sdk
         api-key="539375f3-7b54-4600-b190-fd9efe7ceaf8"
         user-unique-id="5d255f56-12d1-4e87-a618-9ef46737f338"
