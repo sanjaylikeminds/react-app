3b414c7642fee82243a34fcdfde0f63c11327d79
Sanjay Kumar <sanjay.kumar@likeminds.community>
2022-10-20 14:24:09 +0530

    bug fixed

--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -3,18 +3,23 @@ import React from "react";
 export default function Community() {
   return (
     <div className="likemind-community">
+      {/* Logged in user */}
       <likeminds-web-sdk
         api-key="539375f3-7b54-4600-b190-fd9efe7ceaf8"
         user-unique-id="5d255f56-12d1-4e87-a618-9ef46737f338"
         user-name="Test User"
         is-guest="false"
       ></likeminds-web-sdk>
+      {/* Logged in user */}
+
+      {/* Guest user */}
       {/* <likeminds-web-sdk
-        api-key="7d6374e0-9d07-4d85-9a8d-773ae8bbdd4e"
-        user-unique-id="10003"
-        user-name="Sanjay"
-        is-guest="false"
+        api-key="539375f3-7b54-4600-b190-fd9efe7ceaf8"
+        user-unique-id="000000"
+        user-name="Test User"
+        is-guest="true"
       ></likeminds-web-sdk> */}
+      {/* Guest user */}
     </div>
   );
 }
