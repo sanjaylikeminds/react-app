6c39cc7f218a3dcdfde52d52ed1d7a7e5e2a97ac
Sanjay Kumar <sanjay.kumar@likeminds.community>
2022-11-11 16:22:24 +0530

    sdk release updated

--- src/Pages/Chatroom.js
+++ src/Pages/Chatroom.js
@@ -5,13 +5,12 @@ export default function Chatroom() {
 
   return (
     <div>
-      sdfsdf
-      {/* <likemind-sdk
+      <likemind-sdk
         api-key="539375f3-7b54-4600-b190-fd9efe7ceaf8"
         user-unique-id="5d255f56-12d1-4e87-a618-9ef46737f338"
         user-name="Test User"
         is-guest="false"
-      ></likemind-sdk> */}
+      ></likemind-sdk>
       {/* <likemind-sdk></likemind-sdk> */}
     </div>
   );
--- src/Pages/Community.js
+++ src/Pages/Community.js
@@ -5,21 +5,21 @@ export default function Community() {
     <div className="likemind-community">
       {/* Logged in user */}
 
-      {/* <likeminds-sdk
+      <likeminds-sdk
         api-key="539375f3-7b54-4600-b190-fd9efe7ceaf8"
         user-unique-id="5d255f56-12d1-4e87-a618-9ef46737f338"
         user-name="Test User"
         is-guest="false"
-      ></likeminds-sdk> */}
+      ></likeminds-sdk>
       {/* Logged in user */}
 
       {/* Guest user */}
-      <likeminds-sdk
+      {/* <likeminds-sdk
         api-key="539375f3-7b54-4600-b190-fd9efe7ceaf8"
         user-unique-id="xxxxxx"
         user-name="guest"
         is-guest="true"
-      ></likeminds-sdk>
+      ></likeminds-sdk> */}
       {/* Guest user */}
     </div>
   );
